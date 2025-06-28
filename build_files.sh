c37535629dda1399ac6c90f25fcc58837785ee23
Your Name <user@example.com>
2025-06-28 13:27:38 +0200

    1

--- build_files.sh
+++ build_files.sh
@@ -4,11 +4,3 @@
 echo "Building the project..."
 python -m pip install -r requirements.txt
 
-# Make migrations
-echo "Making migrations..."
-python manage.py makemigrations
-python manage.py migrate
-
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
